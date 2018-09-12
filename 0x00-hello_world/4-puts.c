d80ddfa9229209c7a095488fe98a7d4a4f18ade7
stefansilverio <494@holbertonschool.com>
2018-09-12 21:28:17 +0000

    print a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
-/* This code prints a string
- * Main function is starting point
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
