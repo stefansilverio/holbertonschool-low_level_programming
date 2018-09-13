9aee093a10e8fc03b5f58b13f39dfa908fbb100b
stefansilverio <494@holbertonschool.com>
2018-09-13 21:26:40 +0000

    cut out e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch < 'z'; ch++)
+		if (ch == 'e')
+			;
+		else if (ch == 'q')
+			;
+		else
+			putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
