316f88e02f2099138dc8b56c436b7a1a26f135cb
stefansilverio <494@holbertonschool.com>
2018-09-13 22:46:55 +0000

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; ++x)
+		putchar(x);
+
+	putchar('\n');
+	return (0);
+}
