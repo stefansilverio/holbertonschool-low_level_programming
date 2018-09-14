6ccd3bb5eb76624f287661d81c1f46249fa825c6
stefansilverio <494@holbertonschool.com>
2018-09-14 19:19:15 +0000

    write as printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 80);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 80);
 	return (1);
 }
