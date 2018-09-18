8e74477e25a016b619c12bc46996f2bef158955b
stefansilverio <494@holbertonschool.com>
2018-09-18 21:11:37 +0000

    print time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - print every moment of the day
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 23; x++)
+	{
+		for (y = 0; y <= 59; y++)
+		{
+			_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
+			_putchar(':');
+			_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
+		}
+	}
+}
+ 
