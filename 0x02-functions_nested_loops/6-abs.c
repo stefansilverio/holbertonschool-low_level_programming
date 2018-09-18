1276d20b1db9228093edc5d90324e322f59cdde2
stefansilverio <494@holbertonschool.com>
2018-09-18 18:05:03 +0000

    convert to absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _abs - convert to absolute value.
+ *
+ * @i: compute absolute value.
+ *
+ * Return: Always 0.
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+		return (i);
+	else
+	{
+		i = i * (-1);
+		return (i);
+	}
+
+}
+
+
+
+
