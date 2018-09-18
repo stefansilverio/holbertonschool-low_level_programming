97ee047b1d1feabbc5d058c458ba448a6c4e8ced
stefansilverio <494@holbertonschool.com>
2018-09-18 17:05:05 +0000

    check code for letters

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,5 +1,12 @@
 #include "holberton.h"
 
+/**
+ * _isalpha - check the code for upper or lowercase.
+ *
+ * @c: Checks for characters.
+ *
+ * Return: Always 0.
+ */
 int _isalpha(int c)
 {
 	if (((c >= 'A') && (c <= 'Z')) || ((c >= 'a') && (c <= 'z')))
