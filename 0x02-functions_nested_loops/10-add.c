fb82b45766242d792d97454d19f71fecb316ac0d
stefansilverio <494@holbertonschool.com>
2018-09-18 22:48:27 +0000

    add numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * add - adds two ints together.
+ *
+ * @a: one digit.
+ *
+ * @i: second digit.
+ *
+ * Return: Always 0.
+ */
+int add(int a, int i)
+{
+	int sum;
+
+	sum = a + i;
+	return (sum);
+}
