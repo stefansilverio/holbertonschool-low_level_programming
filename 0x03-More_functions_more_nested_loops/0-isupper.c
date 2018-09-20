f9f61b4f1bd8f1a651a8b34bd11d5bfb32415f7d
stefansilverio <494@holbertonschool.com>
2018-09-20 16:23:13 +0000

    check for uppercase letter

---
+++ 0x03-More_functions_more_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * _isupper - function checks for uppercase char.
+ *
+ * @c: letter to be checked.
+ *
+ * Return: Always 0 (Success)
+ */
+int _isupper(int c)
+{
+	int x;
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		if (x == c)
+			return (1);
+		else if (x != c)
+			return (0);
+	}
+	return (0);
+}
