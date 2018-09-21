e1a856bd27efeb52d87637ace7d0abad96105ac9
stefansilverio <494@holbertonschool.com>
2018-09-21 02:01:17 +0000

    print blank lines

--- 0x03-more_functions_nested_loops/6-print_line.c
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -11,12 +11,16 @@ void print_line(int n)
 {
 	int x;
 
-	for (x = 0; x <= n; x++)
+	if (n > 0)
 	{
-		if (n <= 0)
-			break;
-		_putchar('_');
+
+		for (x = 0; x <= n; x++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
+		else
+			_putchar('\n');
 }
 
