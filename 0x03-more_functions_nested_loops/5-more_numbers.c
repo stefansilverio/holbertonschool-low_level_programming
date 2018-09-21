cfb9800fb47d3a3cbd0aba1b8fc1ac64ed82ede4
stefansilverio <494@holbertonschool.com>
2018-09-21 02:04:03 +0000

    print a wall of numbers

---
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - print ten times 0-14.
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y > 9)
+				_putchar(y / 10 + '0');
+			_putchar(y % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
