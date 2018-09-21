73ac627ff28b73ddfbadf918487687e58c9c7723
stefansilverio <494@holbertonschool.com>
2018-09-21 01:29:54 +0000

    determine if character is uppercase

--- 0x03-more_functions_nested_loops/0-isupper.c
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -9,13 +9,7 @@
  */
 int _isupper(int c)
 {
-	int x;
-
-	for (x = 'A'; x <= 'Z'; x++)
-	{
-		if (x == c)
-			return (1);
-		else if (x != c)
-			return (0);
-	}
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
 }
