b8cc7d061ab3a0a1250cada0fb47ea92475b2ab3
stefansilverio <494@holbertonschool.com>
2018-09-21 01:55:58 +0000

    print blank lines

---
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_line - print a straight line across the terminal.
+ *
+ * @n: length of line.
+ *
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int x;
+
+	for (x = 0; x <= n; x++)
+	{
+		if (n <= 0)
+			break;
+		_putchar('_');
+	}
+	_putchar('\n');
+}
+
