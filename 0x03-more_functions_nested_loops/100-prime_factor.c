1d0429b836d09876c86463c281825a66a3e13afe
stefansilverio <494@holbertonschool.com>
2018-09-21 23:44:57 +0000

    find greatest prime factor

---
+++ 0x03-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - return greated common factor.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long factor;
+	long n = 612852475143;
+
+	for (factor = 2; factor < n / 2; factor++)
+	{
+		if ((n % factor) == 0)
+		{
+			n /= factor;
+			factor = 2;
+		}
+	}
+	printf("%lu\n", n);
+	return (0);
+} 
