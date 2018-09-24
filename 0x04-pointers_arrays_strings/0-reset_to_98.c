d92eaff56b15e1d533816a888737eee6db3b9f61
stefansilverio <494@holbertonschool.com>
2018-09-24 19:43:28 +0000

    reset pointer value

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - reset pointer
+ *
+ * pointer value taken
+ *
+ * @n: address to variable n
+ *
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
