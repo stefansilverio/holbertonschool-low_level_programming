8f89157696d94221a56ffca774f5a67ff3df47cc
stefansilverio <494@holbertonschool.com>
2018-09-25 17:48:52 +0000

    print half of a string

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * puts_half - print half of a string.
+ *
+ * @str: string to be cut.
+ *
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int check = 0;
+
+	while (str[i])
+		i++;
+	check = i / 2;
+	if ((i % 2) == 0)
+	{
+		while (check <= i - 1)
+		{
+			_putchar(str[check]);
+			check++;
+
+		}
+	}
+	else
+	{
+		while ((check - 1) <= (i - 1))
+		{
+			_putchar(str[check]);
+			check++;
+		}
+	}
+	_putchar('\n');
+}
