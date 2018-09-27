91a6ab8289ad7149d72f8a5395db2351160d3ac8
stefansilverio <494@holbertonschool.com>
2018-09-27 22:17:36 +0000

    copy n characters of a string

---
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - copy n characters to a buffer
+ *
+ * @dest: destination buffer
+ *
+ * @src: string to be appended
+ *
+ * @n: number of characters to be copied
+ *
+ * Return: Always 0.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0;
+
+	while (src[index] && n > index)
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	return (dest);
+}
