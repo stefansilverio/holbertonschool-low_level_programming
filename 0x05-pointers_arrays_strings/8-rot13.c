96e11cea0d43547c3c3a07e08db7473c184839c0
stefansilverio <494@holbertonschool.com>
2018-09-29 03:06:15 +0000

    encode a string in rot13

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -9,5 +9,21 @@
  */
 char *rot13(char *s)
 {
+	int i = 0;
+
+	while (s[i])
+	{
+		while ((s[i] >= 65) && (s[i] <= 122))
+		{
+			if ((s[i] < 'N') || ((s[i] < 'n') && (s[i] >= 'a')))
+			{
+				s[i] = s[i] + 13;
+				break;
+			}
+			s[i] = s[i] - 13;
+			break;
+		}
+		i++;
+	}
 	return (s);
 }
