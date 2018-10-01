f34692b6b20f34cc496a44d1fa43019d61178420
stefansilverio <494@holbertonschool.com>
2018-10-01 21:14:40 +0000

    locate address of an integer

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -13,11 +13,10 @@ char *_strchr(char *s, char c)
 {
 	int index = 0;
 
-	while ((s[index]) || (s[index] == '\0'))
+	for (index = 0; s[index] >= '\0'; index++)
 	{
 		if (s[index] == c)
 			return (s + index);
-		index++;
 	}
 	return ('\0');
 }
