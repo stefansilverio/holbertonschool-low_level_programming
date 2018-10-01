46f43fe32b055f8e068467c29b761471cc8496c9
stefansilverio <494@holbertonschool.com>
2018-10-01 16:11:49 +0000

    print an integer

---
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * print_number - print an integer.
+ *
+ * @n: integer to be printed.
+ *
+ * Return: Always 0.
+ */
+
+void print_number(int n)
+{
+	if ((n > 9) || (n < -9))
+	{
+		print_number(n / 10);
+		if (n < 0)
+			n = (-n);
+		_putchar((n % 10) + '0');
+	}
+	else
+	{
+		if (n < 0)
+		{
+			_putchar('-');
+			n = (-n);
+		}
+		_putchar(n + '0');
+	}
+}
