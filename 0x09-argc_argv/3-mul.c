ccfdba18bf3b84b56cadf318c46ca5fa6d2b9754
stefansilverio <494@holbertonschool.com>
2018-10-05 03:58:32 +0000

    multiply two numbers

---
+++ 0x09-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the multiple of two numbers
+ *
+ * @argc: argument count
+ *
+ * @argv: arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int storage;
+
+	if (argc < 3)
+	{
+		puts("error\n");
+		return (1);
+	}
+	storage = atoi(argv[argc - 1]) * atoi(argv[argc - 2]);
+	printf("%d\n", storage);
+	return (0);
+}
