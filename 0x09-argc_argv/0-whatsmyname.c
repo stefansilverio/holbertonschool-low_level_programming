c9a0e0b726a1fd745df894928339498caa0c8228
stefansilverio <494@holbertonschool.com>
2018-10-05 03:19:22 +0000

    print the name of a function

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - print program name followed by newline
+ *
+ * @argc: argument count
+ *
+ * @argv: arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int index = 0;
+	(void)argc;
+
+	printf("%s\n", argv[index]);
+
+	return (0);
+}
