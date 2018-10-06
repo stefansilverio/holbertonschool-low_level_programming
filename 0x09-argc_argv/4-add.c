dc14a435e950db69de20e6287c802a3e16debc58
stefansilverio <494@holbertonschool.com>
2018-10-06 19:02:45 +0000

    add string of integers

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -16,25 +16,39 @@ int main(int argc, char *argv[])
 {
 	int storage = 0;
 	int index;
+	int sub_index;
 
 	if (argc < 2)
 	{
 		printf("%d\n", 0);
 		return (0);
 	}
+	printf("hi ");
 
-	for (index = 1; argv[index] != '\0'; index++)
+	for (index = 1; index < argc - 1; index++)
 	{
 		if (atoi(argv[index]) < 0)
 		{
-			puts("Error");
+			printf("Error\n");
 			return (1);
 		}
+		printf("hi ");
+		for (sub_index = 1; *argv[sub_index] != '\0'; sub_index++)
+		{
+			if ((**argv++ > '9') || (**argv++ < '0'))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("hi ");
 	}
 
-	for (index = 1; argv[index] != '\0'; index++)
+	for (index = 1; index < argc - 1; index++)
 		storage += atoi(argv[index]);
 
+	printf("hi");
 	printf("%d\n", storage);
 	return (0);
+
 }
