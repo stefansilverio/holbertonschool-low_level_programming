be31d5e0970e42d22af15941218cb2b062998bfd
stefansilverio <494@holbertonschool.com>
2018-10-09 19:48:33 +0000

    return a copy to a duplicate identical string

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a ptr to duplicate string
+ *
+ * @str: string to duplicate and return
+ *
+ * Return: Always 0
+ */
+char *_strdup(char *str)
+{
+	char *buffer;
+	int index;
+	int size = 0;
+
+	for (index = 0; str[index] != '\0'; index++)
+		size++;
+
+	buffer = malloc(size * sizeof(char));
+
+	index = 0;
+
+	if (buffer == NULL)
+		return (NULL);
+
+	else
+		while (size > index)
+		{
+			*(buffer + index) = *(str + index);
+			index++;
+		}
+	return (buffer);
+}
