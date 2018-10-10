3a107431bd9e8db97be3dab3845e520ea7da46b7
stefansilverio <494@holbertonschool.com>
2018-10-10 05:51:21 +0000

    concatenate arguments

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenate all arguments in program
+ *
+ * @ac: argument count
+ *
+ * @av: ptr to arguments
+ *
+ * Return: Always 0.
+ */
+char *argstostr(int ac, char **av)
+{
+	if ((ac == 0) || (av == NULL))
+	{
+		return (NULL);
+	}
+}
