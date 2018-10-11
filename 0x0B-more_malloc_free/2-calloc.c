4d257ff45e92d1999cc078bc070c98cb623c9c7b
stefansilverio <494@holbertonschool.com>
2018-10-11 21:33:34 +0000

    create calloc

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if ((nmemb == 0) || (size == 0))
 		return (NULL);
 
-	buffer = malloc(nmemb * size);
+	buffer = (void *)malloc(nmemb * size);
 
 	if (buffer == NULL)
 		return (NULL);
