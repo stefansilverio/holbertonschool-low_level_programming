a4f4c64e9eec86cb83de3f5f657022fca817962f
stefansilverio <494@holbertonschool.com>
2018-10-11 00:21:26 +0000

    concatenate all arguments passed to the function

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -13,8 +13,36 @@
  */
 char *argstostr(int ac, char **av)
 {
+	char *str;
+	int row;
+	int column;
+	int index;
+	int len;
+
 	if ((ac == 0) || (av == NULL))
 	{
 		return (NULL);
 	}
+
+	for (row = 0; row < ac; row++)
+		len += strlen(av[row]);
+
+	str = malloc((len + ac + 1) * sizeof(char));
+
+	if (str == NULL)
+		return (NULL);
+
+	for (row = 0; row < ac; row++)
+	{
+		column = 0;
+		while (ac[row][column])
+		{
+			str[index] = av[row][column];
+			column++;
+			index++;
+		}
+		str[index] = '\n';
+		str[index + 1] = '\0';
+	}
+	return (str);
 }
