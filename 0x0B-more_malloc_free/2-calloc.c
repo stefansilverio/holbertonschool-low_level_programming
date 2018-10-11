be4070bd235935a6908a42a8c92e4472366f4f1d
stefansilverio <494@holbertonschool.com>
2018-10-11 21:45:12 +0000

    allocate memory for an arrary

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -14,14 +14,26 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *buffer;
+	int buffer_size;
+	int index = 0;
+
+	buffer_size = nmemb * size;
 
 	if ((nmemb == 0) || (size == 0))
 		return (NULL);
 
-	buffer = (void *)malloc(nmemb * size);
+	buffer = malloc(nmemb * size);
 
 	if (buffer == NULL)
 		return (NULL);
+
+	while (buffer_size--)
+	{
+		buffer[index] = 0;
+		index++;
+	}
+
+	buffer = (void *)buffer;
 
 	return (buffer);
 }
