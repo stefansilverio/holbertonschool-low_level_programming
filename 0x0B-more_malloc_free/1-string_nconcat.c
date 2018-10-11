7526ece56cf9a541c9319cabdeb67ce991ebe9f5
stefansilverio <494@holbertonschool.com>
2018-10-11 20:22:27 +0000

    concatenate strings

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -28,7 +28,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s2[index_2] != '\0') /* space for src */
 		index_2++;
 
-	if (n >= index_2)
+	if (n > index_2)
 		n = index_2;
 
 	buffer = malloc((index_1 + n + 1) * sizeof(char));
