46fe93203b879f9d39ab51cad0faf0d4132fdc57
stefansilverio <494@holbertonschool.com>
2018-10-15 20:33:27 +0000

    function like macro

--- 0x0C-preprocessor/3-function_like_macro.h
+++ 0x0C-preprocessor/3-function_like_macro.h
@@ -1,19 +1,6 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-/**
- * ABS - absolute value
- *
- * @x: number to take abs value of
- *
- * Return: Always 0
- */
-int ABS(int x)
-{
-	if (x < 0)
-		x *= (-1);
-
-	return (x);
-}
+#define ABS(x) ((x) < 0 ? -(x) : (x))
 
 #endif
