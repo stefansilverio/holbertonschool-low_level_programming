14166caa1bcedc040935128fd9700823d7efac9e
stefansilverio <494@holbertonschool.com>
2018-10-15 23:27:46 +0000

    dog details

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+/**
+ * struct dog - dog details
+ * @name: name of dog
+ * @owner: dog owner
+ * @age: dog age
+ *
+ * Description - this struct provides dog details
+ */
 struct dog
 {
 	char *name;
