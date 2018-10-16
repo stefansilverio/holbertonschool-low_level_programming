30b314059b1935d47f2da1de1cf7ba02d6c25957
stefansilverio <494@holbertonschool.com>
2018-10-16 23:04:27 +0000

    create new dogs

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -19,11 +19,9 @@ dog_t *new_dog(char *name, float age, char *owner) /* return ptr to new dog */
 	int index_2;
 	int fill_1;
 	int fill_2;
-	char *new_name;
-	char *new_owner;
 	dog_t *new_dog;
 
-	new_dog = malloc(sizeof(dog_t));
+	new_dog = malloc(sizeof(dog_t)); /* passing address to new_dog */
 
 	if (new_dog == NULL)
 		return (NULL);
@@ -34,35 +32,35 @@ dog_t *new_dog(char *name, float age, char *owner) /* return ptr to new dog */
 	for (index_2 = 0; owner[index_2]; index_2++)
 		;
 
-	new_name = malloc((index_1 + 1) * sizeof(char));
+	new_dog->name = malloc((index_1 + 1) * sizeof(char));
 
-	if (new_name == NULL)
+	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
 
-	new_owner = malloc((index_2 + 1) * sizeof(char));
+	new_dog->owner = malloc((index_2 + 1) * sizeof(char));
 
-	if (new_owner == NULL)
+	if (owner == NULL)
 	{
-		free(new_name);
+		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
 
 	for (fill_1 = 0; index_1 > fill_1; fill_1++)
-		new_name[fill_1] = name[fill_1];
+		new_dog->name[fill_1] = name[fill_1];
 
-	new_name[fill_1] = '\0';
+	new_dog->name[fill_1] = '\0';
 
 	for (fill_2 = 0; index_2 > fill_2; fill_2++)
-		new_owner[fill_2] = owner[fill_2];
+		new_dog->owner[fill_2] = owner[fill_2];
 
-	new_owner[fill_2] = '\0';
+	new_dog->owner[fill_2] = '\0';
 
-	new_dog->name = new_name;
-	new_dog->owner = new_owner;
+	new_dog->name = name;
+	new_dog->owner = owner;
 	new_dog->age = age;
 
 	return (new_dog);
