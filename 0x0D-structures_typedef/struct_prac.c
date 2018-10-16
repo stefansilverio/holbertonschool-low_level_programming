5d0d1dd21abbc5e8e419c9ee04e0a795693c778d
stefansilverio <494@holbertonschool.com>
2018-10-16 01:54:49 +0000

    struct practice

--- 0x0D-structures_typedef/struct_prac.c
+++ 0x0D-structures_typedef/struct_prac.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-struct Users
+struct User
 {
 	char *name;
 	char *email;
@@ -12,7 +12,16 @@ struct User *new_user(char *name, char *email, int age)
 {
 	struct User *user;
 
-	return user;
+	user = malloc(sizeof(struct User));
+
+	if (user == NULL)
+		return (NULL);
+
+	user->name = name;
+	user->email = email;
+	user->age = age;
+
+	return (user);
 }
 
 int main(void)
@@ -20,6 +29,12 @@ int main(void)
 	struct User *user;
 
 	user = new_user("Foo", "foo@foo.bar", 98);
+
 	if (user == NULL)
 		return (1);
 
+	printf("User %s created !\n", user->name);
+	printf("His email is: %s\n", user->email);
+	printf("And he is %d years old\n", user->age);
+	return(0);
+}
