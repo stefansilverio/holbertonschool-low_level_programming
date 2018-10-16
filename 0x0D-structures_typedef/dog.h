42ed2a31619306eb1977d58ddaed4dcfb9407fe6
stefansilverio <494@holbertonschool.com>
2018-10-16 22:06:51 +0000

    new header file

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -16,12 +16,13 @@ struct dog
 	char *owner;
 	float age;
 };
+
 typedef struct dog dog_t;
 
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
