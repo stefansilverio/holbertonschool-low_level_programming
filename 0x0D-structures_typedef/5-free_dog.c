9cb61a1974e8d3bb287b7f7df774381e09c2b00e
stefansilverio <494@holbertonschool.com>
2018-10-16 22:06:01 +0000

    number of dogs freed

---
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - free some dogs
+ *
+ * @d: pointer to a dog struct
+ *
+ * Return: Always 0
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
