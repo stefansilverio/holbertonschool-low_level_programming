78e6a2b77672c2914a8fa3c535a6342fcbbe972d
stefansilverio <494@holbertonschool.com>
2018-10-16 21:08:33 +0000

    create a new dog

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -3,9 +3,15 @@
 #include "dog.h"
 
 /**
+ * new_dog - return a ptr to a new dog
  *
+ * @name: name of new dog
  *
+ * @owner: new owner
  *
+ * @age: new age
+ *
+ * Return: ptr to new dog
  */
 
 dog_t *new_dog(char *name, float age, char *owner) /* return ptr to new dog */
@@ -16,34 +22,34 @@ dog_t *new_dog(char *name, float age, char *owner) /* return ptr to new dog */
 	int fill_2 = 0;
 	char *new_name;
 	char *new_owner;
-	float new_age;
 	dog_t *new_dog;
 
-	new_dog = malloc(sizeof(struct dog_t));
+	new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
 		return (NULL);
 
 	for (index_1 = 0; name[index_1]; index_1++)
-	{
-		new_name = malloc((index_1) * sizeof(char));
-
-		if (new_name == NULL)
-		{
-			new_name = '\0';
-			return (NULL);
-		}
-	}
+		;
 
 	for (index_2 = 0; owner[index_2]; index_2++)
-	{
-		new_owner = malloc((index_2) * sizeof(char));
+		;
 
-		if (new_owner == NULL)
-		{
-			new_owner = '\0';
-			return (NULL);
-		}
+	new_name = malloc((index_1) * sizeof(char));
+
+	if (new_name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+
+	new_owner = malloc((index_2) * sizeof(char));
+
+	if (new_owner == NULL)
+	{
+		free(new_name);
+		free(new_dog);
+		return (NULL);
 	}
 
 	while (index_1 < fill_1)
@@ -58,7 +64,9 @@ dog_t *new_dog(char *name, float age, char *owner) /* return ptr to new dog */
 		fill_2++;
 	}
 
-	new_age = age;
+	new_dog->name = new_name;
+	new_dog->owner = new_owner;
+	new_dog->age = age;
 
 	return (new_dog);
 }
