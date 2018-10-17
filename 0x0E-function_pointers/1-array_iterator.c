666fdf4ab87f5633bb030542b61b03a7905764fc
stefansilverio <494@holbertonschool.com>
2018-10-17 23:45:42 +0000

    execute on each element

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -17,10 +17,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int index = 0;
 
-	if ((size == 0) || (action == NULL))
+	if ((size == 0) || (array  == NULL) || (action == NULL))
 		return;
 
-	while (size--)
+	while (size > index)
 	{
 		action(array[index]);
 		index++;
