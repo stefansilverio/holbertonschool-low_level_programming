f80227bf19ad519d88f76eea464ff99da1bffab1
stefansilverio <494@holbertonschool.com>
2018-10-17 18:03:59 +0000

    execute function on array

---
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - execute on each element of array
+ *
+ * @array: array given
+ *
+ * @size: size of array
+ *
+ * @action: ptr to fn
+ *
+ * Return: always 0
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int index = 0;
+
+	while (size--)
+	{
+		action(array[index]);
+		index++;
+	}
+}
