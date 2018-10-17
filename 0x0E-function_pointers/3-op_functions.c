677e4e1799caa995d6f6f15e0bab18948ca4f542
stefansilverio <494@holbertonschool.com>
2018-10-17 22:46:24 +0000

    implementation

--- 0x0E-function_pointers/3-op_functions.c
+++ 0x0E-function_pointers/3-op_functions.c
@@ -5,13 +5,9 @@
 /**
  * op_add - returns the sum of a and b
  *
- * op_sub: returns the difference of a and b
+ * @a: num1
  *
- * op_mul: returns the product of a and b
- *
- * op_div: returns the result of the division of a by b
- *
- * op_mod: returns the remainder of the division of a by b
+ * @b: num2
  *
  * Return: Always 0
  */
@@ -19,22 +15,55 @@
 	{
 		return (a + b);
 	}
-
+/**
+* op_sub - returns the difference of a and b
+*
+* @a: num1
+*
+* @b: num2
+*
+* Return: Always 0
+*/
 	int op_sub(int a, int b)
 	{
 		return (a - b);
 	}
 
+/**
+ * op_mul - returns the product of a and b
+ *
+ * @a: num1
+ *
+ * @b: num2
+ *
+ * Return: Always 0
+ */
 	int op_mul(int a, int b)
 	{
 		return (a * b);
 	}
-
+/**
+ * op_div - returns the result of the division of a by b
+ *
+ * @a: num1
+ *
+ * @b: num2
+ *
+ * Return: Always 0
+ */
 	int op_div(int a, int b)
 	{
 		return (a / b);
 	}
-
+/**
+ * op_mod - returns remainder of division
+ *
+ * @a: num1
+ *
+ * @b: num2
+ *
+ * Return: Always 0
+ */
 	int op_mod(int a, int b)
 	{
 		return (a % b);
