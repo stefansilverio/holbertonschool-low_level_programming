60dda4f765fc327e6121b949d9fb82381da8fe96
stefansilverio <494@holbertonschool.com>
2018-10-17 22:22:50 +0000

    calculator

---
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - select correct function
+ *
+ * @s: operator passed into program
+ *
+ * Return: Always 0
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	int i = 0;
+
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	while (i < 6)
+	{
+		if (*s == ops[i].op[0])
+			return (ops[i].f);
+		i++;
+	}
+
+	return (NULL);
+}
---
+++ 0x0E-function_pointers/3-main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - call 3-get_op_func
+ *
+ * @argc: argument count
+ *
+ * @argv: argument array
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = argv[2];
+
+	if ((num2 == 0) && ((*op == '/') || (*op == '%')))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	if (get_op_func(op))
+	{
+		result = get_op_func(op)(num1, num2);
+		printf("%d\n", result);
+		return (result);
+	}
+
+	printf("Error\n");
+	exit (99);
+	return (0);
+}
