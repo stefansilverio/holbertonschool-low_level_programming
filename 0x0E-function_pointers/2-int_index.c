7482d9d7183260765061a223718a159cbcabcc8e
stefansilverio <494@holbertonschool.com>
2018-10-17 23:54:36 +0000

    search a string

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -17,6 +17,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int index = 0;
 
+	if ((array == NULL) || (size == 0) || (cmp ==  NULL))
+		return (-1);
+
 	if (size <= 0)
 		return (-1);
 
