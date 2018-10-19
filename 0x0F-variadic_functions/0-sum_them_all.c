7b3829091b0878f2f8293e75ec4bc6308e591b0c
stefansilverio <494@holbertonschool.com>
2018-10-19 20:32:30 +0000

    sum all arguments passed to fn

---
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sum all args
+ *
+ * @n: number of args passed in
+ *
+ * Return: Always 0
+ */
+int sum_them_all(const int n, ...)
+{
+	va_list valist;
+	int sum = 0;
+	int i;
+
+	if (n == 0)
+		return (0);
+
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
+
+	va_end(valist);
+
+	return (sum);
+}
