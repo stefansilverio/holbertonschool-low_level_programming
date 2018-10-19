bff63723411bfe7b0fcd6702f753ff4ecd2f7307
stefansilverio <494@holbertonschool.com>
2018-10-19 21:18:29 +0000

    print numbers passed to function

---
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print numbers followed by a newline
+ *
+ * @separator: separate numbers
+ *
+ * @n: number of args
+ *
+ * Return: Always 0
+ */
+void print_numbers(const char *separator, const int n, ...)
+{
+	va_list valist;
+	int i;
+
+	va_start(valist, n); /* n is # of args */
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(valist, int));
+		if ((i+1 == n) || (separator == NULL))
+			continue;
+		printf("%s", separator);
+	}
+
+	va_end(valist);
+	printf("\n");
+}
