598dbf5e441b194ff81872a21b3cb272a39985e6
stefansilverio <494@holbertonschool.com>
2018-10-20 20:23:53 +0000

    new header file

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -2,9 +2,9 @@
 #define HEADER_FILE
 
 int _putchar(char c);
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
+int sum_them_all(const int n, ...);
+void print_numbers(const char *separator, const int n, ...);
+void print_strings(const char *separator, const int n, ...);
 void print_all(const char * const format, ...);
 
 #endif
