26c98c0715bedd398dff464e5bf46f04a031d1bb
stefansilverio <494@holbertonschool.com>
2018-10-25 22:17:55 +0000

    insert new node at start of list

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -12,11 +12,16 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	int len = 0;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->str = strdup(str);
+	while (str[len] != '\0')
+		len++;
+
+	new_node->len = len;
 	new_node->next = (*head);
 	(*head) = new_node;
 	return (new_node);
