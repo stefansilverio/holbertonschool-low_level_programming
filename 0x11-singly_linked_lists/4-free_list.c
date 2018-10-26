1eaf2e51b318c5fc0f050530274fac09ee3601d0
stefansilverio <494@holbertonschool.com>
2018-10-26 22:05:26 +0000

    free a list_t

--- 0x11-singly_linked_lists/4-free_list.c
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -9,10 +9,13 @@
  */
 void free_list(list_t *head)
 {
-	if (head == NULL)
-		return;
-	else if (head->next)
-		free_list(head->next);
-	free(head->str);
-	free(head);
+	list_t *tmp = 0;
+
+	while (head)
+	{
+		tmp = head;
+		free(tmp->str);
+		free(tmp);
+		head = head->next;
+	}
 }
