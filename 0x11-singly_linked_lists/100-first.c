384a9795eb2edc01b3ff111a8908100482b6fec7
stefansilverio <494@holbertonschool.com>
2018-10-26 03:12:01 +0000

    executing a function before main

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void myStartupFun(void) __attribute__ ((constructor));
+
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
