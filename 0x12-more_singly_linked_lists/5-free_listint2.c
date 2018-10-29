c14a625bc24949079af838ef74bc34327cf927c2
stefansilverio <494@holbertonschool.com>
2018-10-29 23:03:48 +0000

    free a linked list

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * free_listint2 - free a linked list
+ * @head: ptr to ptr to first node
+ * Return: none
+ */
 void free_listint2(listint_t **head)
 {
 	listint_t *free_head;
