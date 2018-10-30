141236e030d3aecadfea59e5414655e8aab5e1ea
stefansilverio <494@holbertonschool.com>
2018-10-30 16:55:08 +0000

    free a linked list

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -13,10 +13,11 @@ void free_listint2(listint_t **head)
 
 	if (*head == NULL)
 		return;
+
 	while (*head)
 	{
 		free_head = *head;
-		*head = free_head->next;
 		free(free_head);
+		*head = (*head)->next;
 	}
 }
