01c828615abb766a1ab2f870d4843cc05c9f90e5
stefansilverio <494@holbertonschool.com>
2018-10-31 01:28:38 +0000

    delete node

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -34,10 +34,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		tmp = tmp->next; /* sitting on node to delete */
 	}
 	if (!tmp)
-	{
-		free(tmp);
-		return(1);
-	}
+		return (-1);
 	while (move < count)
 	{
 		move++;
