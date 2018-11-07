83f70cc71a4f84da833d634fa89a5858bfb631c9
stefansilverio <494@holbertonschool.com>
2018-11-07 06:06:13 +0000

    copy one files content into another

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -12,51 +12,63 @@ int main(int argc, char *argv[])
 	int status = 0;
 	int count = 0;
 
-	buffer = malloc(sizeof(char) * 1024);
-
+	buffer = malloc(sizeof(char) * 1024); /* allocate buffer size */
 	if (buffer == NULL)
 		return (-1);
-
 	if (argc != 3)
 	{
-		printf("Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	fd1 = open(argv[1], O_WRONLY | O_TRUNC, 0664);
-
+	fd1 = open(argv[1], O_RDONLY); /* if file exists truncate */
 	if (fd1 == -1)
 	{
-		printf("Error: Can't read from file NAME_OF_THE_FILE");
+		puts("1");
+		dprintf(STDERR_FILENO, "Error: Can't read from file incitatous\n");
 		exit(98);
 	}
-
-	count = read(fd1, buffer, 1024);
-
+	count = read(fd1, buffer, 1024); /* read from first file into buffer */
 	if (count == -1)
 	{
 		exit(98);
-		printf("Error: Can't read from file NAME_OF_THE_FILE");
+		puts("2");
+		dprintf(STDERR_FILENO, "Error: Can't read from file incitatous\n");
 	}
-
-	fd2 = open(argv[2], O_WRONLY | O_TRUNC);
-
+	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664); /* open second fi */
 	if (fd2 == -1)
 	{
-		printf("Error: Can't read from file NAME_OF_THE_FILE");
+		puts("4");
+		dprintf(STDERR_FILENO, "Error: Can't read from file Incitatous\n");
 		exit(98);
 	}
-
-	status = write(fd2, buffer, 1024);
-
+	status = write(fd2, buffer, 1024); /* write from buffer to second file */
+	write(fd2, "\n", 1);
 	if (status == -1)
 	{
-		printf("Error: Can't write to NAME_OF_THE_FILE");
-		close(fd1);
-		close(fd2);
+		puts("4");
+		dprintf(STDERR_FILENO, "Error: Can't write to Incitatous\n");
+		if (close(fd1) == -1)
+		{
+			exit(100);
+			dprintf(STDERR_FILENO, "Error: Can't close fd fd1\n");
+		}
+		if (close(fd2) == -1)
+		{
+			exit(100);
+			dprintf(STDERR_FILENO, "Error: Can't close fd fd2\n");
+		}
+		free(buffer);
 		exit(99);
 	}
-		close(fd1);
-		close(fd2);
-		return (0);
-}
+	if (close(fd1) == -1)
+	{
+		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd fd1\n");
+	}
+	if (close(fd2) == -1)
+	{
+		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd fd2\n");
+	}
+	free(buffer);
+	return (0); }
