d47bf7bfe13f6f02106829f04d2b70c0a7fed6e6
stefansilverio <494@holbertonschool.com>
2018-11-07 06:53:16 +0000

    copy over file contents

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -4,7 +4,7 @@
 #include "holberton.h"
 #include <unistd.h>
 
-void closed(char *buffer, int status, int fd1, int fd2);
+void closed(int status, int fd1, int fd2);
 
 /**
  * main - cp file contents into other file
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 
 	buffer = malloc(sizeof(char) * 1024); /* allocate buffer size */
 	if (buffer == NULL)
-		return (-1);
+		exit(98);
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
@@ -35,35 +35,39 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file incitatous\n");
 		exit(98);
 	}
-	count = read(fd1, buffer, 1024); /* read from first file into buffer */
-	if (count == -1)
-	{
-		exit(98);
-		puts("2");
-		dprintf(STDERR_FILENO, "Error: Can't read from file incitatous\n");
-	}
-	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664); /* open second fi */
+	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664); /* open fi */
 	if (fd2 == -1)
 	{
 		puts("4");
 		dprintf(STDERR_FILENO, "Error: Can't read from file Incitatous\n");
 		exit(98);
 	}
-	status = write(fd2, buffer, 1024); /* write from buffer to second file */
+	count = read(fd1, buffer, 1024); /* read from first file into buffer */
+	while (count != 0)
+	{
+		count = read(fd1, buffer, 1024); /* read from first file into buffer */
+		if (count == -1)
+		{
+			exit(98);
+			puts("2");
+			dprintf(STDERR_FILENO, "Error: Can't read from file incitatous\n");
+		}
+		status = write(fd2, buffer, 1024); /* write from buffer to second file */
+	}
 	write(fd2, "\n", 1);
-	closed(buffer, status, fd1, fd2);
+	free(buffer);
+	closed(status, fd1, fd2);
 	return (0);
 }
 
 /**
  * closed - close file descriptors and free buffers
- * @buffer: buffer to free
  * @status: status of write call
  * @fd1: file descriptor 1
  * @fd2: file descriptor 2
  * Return: Nothing
  */
-void closed(char *buffer, int status, int fd1, int fd2)
+void closed(int status, int fd1, int fd2)
 {
 	if (status == -1)
 	{
@@ -79,7 +83,6 @@ void closed(char *buffer, int status, int fd1, int fd2)
 			exit(100);
 			dprintf(STDERR_FILENO, "Error: Can't close fd fd2\n");
 		}
-		free(buffer);
 		exit(99);
 	}
 	if (close(fd1) == -1)
@@ -87,10 +90,11 @@ void closed(char *buffer, int status, int fd1, int fd2)
 		exit(100);
 		dprintf(STDERR_FILENO, "Error: Can't close fd fd1\n");
 	}
+	close(fd1);
 	if (close(fd2) == -1)
 	{
 		exit(100);
 		dprintf(STDERR_FILENO, "Error: Can't close fd fd2\n");
 	}
-	free(buffer);
+	close(fd2);
 }
