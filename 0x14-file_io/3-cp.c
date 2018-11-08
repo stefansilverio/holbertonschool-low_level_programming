f108f210b2d2de0388dd6cd9796494d1b573d627
stefansilverio <494@holbertonschool.com>
2018-11-08 06:01:32 +0000

    copy one file to another

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -4,9 +4,9 @@
 #include <fcntl.h>
 #include "holberton.h"
 
-void no_read_create_f1(int fd1, char *arg, char *buffer);
+void no_read_f1(char *arg, char *buffer);
+void no_write_f2(char *arg, char *buffer);
 void close_fail(int fd1, char *buffer);
-
 /**
  * main - copy content to another file
  * @argc: argument count
@@ -16,7 +16,7 @@ void close_fail(int fd1, char *buffer);
 int main(int argc, char *argv[])
 {
 	int fd1, fd2;
-	int  status_read = 1;
+	int status_read = 1;
 	int status_write = 0;
 	char *buffer = NULL;
 
@@ -31,27 +31,27 @@ int main(int argc, char *argv[])
 	fd1 = open(argv[1], O_RDONLY);
 
 	if (fd1 == -1)
-		no_read_create_f1(fd1, argv[1], buffer);
+		no_read_f1(argv[1], buffer);
 
 	fd2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 
 	if (fd2 == -1)
-		no_read_create_f1(fd2, argv[2], buffer);
+		no_write_f2(argv[2], buffer);
 
 	while (status_read != 0)
 	{
 		status_read = read(fd1, buffer, 1024);
 
 		if (status_read == -1)
-			no_read_create_f1(fd1, argv[1], buffer);
+			no_read_f1(argv[1], buffer);
 
-		status_write = write(fd2, buffer, 1024);
+		if (status_read == 0)
+			break;
+
+		status_write = write(fd2, buffer, status_read);
+
 		if (status_write == -1)
-		{
-			free(buffer);
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
+			no_write_f2(argv[2], buffer);
 	}
 
 	free(buffer);
@@ -65,20 +65,29 @@ int main(int argc, char *argv[])
 }
 
 /**
- * no_read_create_f1 - exit if can't open file_from
- * @fd1: file descriptor 1
- * @arg: name of file to print
+ * no_read_f1 - exit if can't open file_from
  * @buffer: buffer to free
+ * @arg: name of file to print
  * Return: Nothing
  */
-void no_read_create_f1(int fd1, char *arg, char *buffer)
+void no_read_f1(char *arg, char *buffer)
 {
-	if (fd1 == -1)
-	{
-		free(buffer);
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", arg);
-		exit(98);
-	}
+	free(buffer);
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", arg);
+	exit(98);
+}
+
+/**
+ * no_write_f2 - exit if can't write
+ * @arg: filename
+ * @buffer: buffer to free
+ * Return: nothing
+ */
+void no_write_f2(char *arg, char *buffer)
+{
+	free(buffer);
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", arg);
+	exit(99);
 }
 
 /**
@@ -89,10 +98,7 @@ void no_read_create_f1(int fd1, char *arg, char *buffer)
  */
 void close_fail(int fd1, char *buffer)
 {
-	if (fd1 == -1)
-	{
-		free(buffer);
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
-		exit(100);
-	}
+	free(buffer);
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+	exit(100);
 }
