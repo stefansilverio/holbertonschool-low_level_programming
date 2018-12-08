e4240b5c06903b79e42fc225735795f2942a5313
stefansilverio <494@holbertonschool.com>
2018-12-08 21:35:47 +0000

    get node at given index

--- 0x16-doubly_linked_lists/5-get_dnodeint.c
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -14,5 +14,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		curr = curr->next;
 		count++;
 	}
+	if ((curr == NULL) && (count < index))
+	{
+		return (NULL);
+	}
+
 	return (curr);
 }
