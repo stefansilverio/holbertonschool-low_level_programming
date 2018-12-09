4b2208ed1f8900f380321bf7a103a9ec0b8000c6
stefansilverio <494@holbertonschool.com>
2018-12-09 00:52:09 +0000

    insert new node

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,6 +14,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *curr = NULL;
 	dlistint_t *tmp = NULL;
 
+	if (h == NULL)
+		return (NULL);
+
 	new_node = malloc(sizeof(dlistint_t));
 
 	if (new_node == NULL)
@@ -21,7 +24,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	new_node->n = n;
 
-	if ((h == NULL) || (*h == NULL) || (idx == 0))
+	if ((*h == NULL) || (idx == 0))
 	{
 		free(new_node);
 		return (add_dnodeint(h, n)); /* this fn returns NULL if h is NULL */
