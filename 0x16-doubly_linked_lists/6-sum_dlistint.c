864a7866137e598abc151dc4b087e4ab7ed9be4e
stefansilverio <494@holbertonschool.com>
2018-12-09 00:01:48 +0000

    sum all elements in linked list

--- 0x16-doubly_linked_lists/6-sum_dlistint.c
+++ 0x16-doubly_linked_lists/6-sum_dlistint.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+ * sum_dlistint - sum all elements in list
+ * @head: ptr to first node
+ * Return: sum of all data
+ */
 int sum_dlistint(dlistint_t *head)
 {
 	dlistint_t *curr = NULL;
