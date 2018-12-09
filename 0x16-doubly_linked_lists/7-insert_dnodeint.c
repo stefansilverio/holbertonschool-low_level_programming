3516af1748a20afdb2660b0327f85c9432e3a8e5
stefansilverio <494@holbertonschool.com>
2018-12-09 21:22:32 +0000

    insert node in list

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,43 +10,49 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int pos = 0;
-	dlistint_t *new_node = NULL;
-	dlistint_t *curr = NULL;
-	dlistint_t *tmp = NULL;
+	dlistint_t *new_node = NULL, *curr = NULL, *tmp = NULL;
 
 	if (h == NULL)
 		return (NULL);
-
 	new_node = malloc(sizeof(dlistint_t));
-
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	if ((*h == NULL) || (idx == 0))
+	if (*h == NULL)
 	{
 		free(new_node);
-		return (add_dnodeint(h, n)); /* this fn returns NULL if h is NULL */
+		return (add_dnodeint(h, n)); /* creates node with single list */
 	}
-
+	if (idx == 0) /* what if curr->next == NULL */
+	{
+		tmp = *h;
+		*h = new_node;
+		new_node->prev = NULL;
+		new_node->next = tmp;
+		return (new_node);
+	}
 	curr = *h;
-
-	while ((pos < idx) && (curr->next))
+	while ((pos + 1 < idx) && (curr->next))
 	{
 		curr = curr->next;
 		pos++;
 	}
-	if ((pos == idx) && (curr->next))
+	if ((pos + 1 == idx) && (curr->next)) /* middle case */
 	{
 		tmp = curr->next;
 		curr->next = new_node;
 		new_node->prev = curr;
 		new_node->next = tmp;
+		tmp->prev = new_node;
 		return (new_node);
 	}
-	else if (((pos + 1 == idx) || (pos == idx)) && (!curr->next))
+	else if ((pos + 1 == idx) && (curr->next == NULL))
 	{
-		free(new_node);
-		return (add_dnodeint_end(h, n));
+		tmp = curr->prev;
+		tmp->next = new_node;
+		new_node->prev = tmp;
+		new_node->next = NULL;
+		return (new_node);
 	}
 	return (NULL); /* if curr->next is null */
 }
