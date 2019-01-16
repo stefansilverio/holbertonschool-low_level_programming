4e243768bd0657fff27d3b1a3ef115d5acbd6837
stefansilverio <494@holbertonschool.com>
2019-01-16 01:34:18 +0000

    generate key for index

--- 0x19-hash_tables/2-key_index.c
+++ 0x19-hash_tables/2-key_index.c
@@ -1,5 +1,14 @@
 #include "hash_tables.h"
 
+/**
+ * key_index - generate key for given index
+ *
+ * @key: key to hash
+ *
+ * @size: size of hash table
+ *
+ * Return: index
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash, index = 0;
