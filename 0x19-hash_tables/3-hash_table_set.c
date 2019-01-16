3256e7f557c744819339da0edf8dbe54fb5d8187
stefansilverio <494@holbertonschool.com>
2019-01-16 01:38:54 +0000

    set hash table

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -1,6 +1,16 @@
 #include "hash_tables.h"
-#include <string.h>
 
+/**
+ * hash_table_set - set new node in hash table
+ *
+ * @ht: hash table
+ *
+ * @key: key to hash
+ *
+ * @value: value to store
+ *
+ * Return: exit status
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int hash, index;
@@ -8,17 +18,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (*key == '\0')
 		return (0);
-
-	if ((curr = malloc(sizeof(hash_node_t))) == NULL)
+	curr = malloc(sizeof(hash_node_t));
+	if (curr == NULL)
 		return (0);
-	if ((mov = malloc(sizeof(hash_node_t))) == NULL)
+	mov = malloc(sizeof(hash_node_t));
+	if (mov == NULL)
 		return (0);
 
-	hash = hash_djb2((const unsigned char*)key);
+	hash = hash_djb2((const unsigned char *)key);
 	index = hash % (ht->size);
-
-	curr->key = (char*) strdup(key);
-	curr->value = (char*) strdup(value);
+	curr->key = (char *) strdup(key);
+	curr->value = (char *) strdup(value);
 	curr->next = NULL;
 
 	if (ht->array[index] != 0)
@@ -38,7 +48,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[index] = curr; /* set new_node as head */
 		curr->next = mov;
 	}
-
 	else
 		ht->array[index] = curr;
 
