ab4fc0cb5e24111f9fb9417cb305f27fbd3cafef
stefansilverio <494@holbertonschool.com>
2019-01-16 01:45:53 +0000

    print hash table

---
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print a hash table
+ *
+ * @ht: hash table
+ *
+ * Return: Nothing
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int idx = 0;
+	hash_node_t  *new_node = NULL;
+	int flag = 0;
+
+	printf("{");
+	for (; idx < (ht->size); idx++)
+	{
+		if ((ht->array[idx]) != 0)
+		{
+			new_node = ht->array[idx];
+
+			while (new_node)
+			{
+				if (flag)
+					printf(", ");
+				printf("'%s': '%s'", new_node->key, new_node->value);
+				new_node = new_node->next;
+				flag = 1;
+			}
+		}
+		else
+			continue;
+	}
+	printf("}\n");
+}
