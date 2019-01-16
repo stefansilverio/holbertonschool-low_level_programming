cbed82699179f1a3e6a53a599383225fb9a73785
stefansilverio <494@holbertonschool.com>
2019-01-16 02:17:00 +0000

    retrieves value

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -15,17 +15,19 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int hash;
 	int index = 0;
 
+	if ((ht == NULL) or (key == NULL))
+		return (NULL);
+
 	hash = hash_djb2((const unsigned char *)key);
 	index = hash % (ht->size);
 
 	if (ht->array[index] == 0)
 		return (NULL);
-	else
-		while (ht->array[index])
-		{
-			if (strcmp(key, ht->array[index]->key) == 0)
-				return (ht->array[index]->value);
-			ht->array[index] = ht->array[index]->next;
-		}
+	while (ht->array[index])
+	{
+		if (strcmp(key, ht->array[index]->key) == 0)
+			return (ht->array[index]->value);
+		ht->array[index] = ht->array[index]->next;
+	}
 	return (NULL);
 }
