1980cbe549ca7388dfbf836e43656e6847206b4f
stefansilverio <494@holbertonschool.com>
2019-01-16 04:12:06 +0000

    free data

--- 0x19-hash_tables/6-hash_table_delete.c
+++ 0x19-hash_tables/6-hash_table_delete.c
@@ -12,12 +12,11 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned long int idx = 0;
 	hash_node_t *curr = NULL, *new_node = NULL;
 
-	new_node = ht->array[idx];
-	curr = new_node;
+	new_node = NULL;
+	curr = ht->array[idx];
 
-	while (idx < (ht->size))
+	for (; idx < (ht->size); idx++)
 	{
-		idx++;
 		if ((curr) == 0)
 			continue;
 		else
@@ -25,10 +24,13 @@ void hash_table_delete(hash_table_t *ht)
 			while (curr)
 			{
 				new_node = curr->next;
+				free(curr->key);
+				free(curr->value);
 				free(curr);
 				curr = new_node;
 			}
 		}
 	}
+	free(ht->array);
 	free(ht);
 }
