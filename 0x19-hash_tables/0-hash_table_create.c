4f7dc7d01684036c613ea9f954c6f6c34f3cec90
stefansilverio <494@holbertonschool.com>
2019-01-16 01:28:58 +0000

    create hash table

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -1,14 +1,25 @@
  #include "hash_tables.h"
 
+/**
+ * hash_table_create - create a hash table
+ *
+ * @size: size of hash table
+ *
+ * Return: hash_table_t
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
 
-	if ((new_table = malloc(sizeof(hash_table_t))) == NULL) /* malloc for hash table */
-		return NULL;
+	new_table = malloc(sizeof(hash_table_t));
 
-	if ((new_table->array = calloc(size, sizeof(hash_node_t*))) == NULL)
-		return NULL;
+	if (new_table == NULL)
+		return (NULL);
+
+	new_table->array = calloc(size, sizeof(hash_node_t *));
+
+	if (new_table->array == NULL)
+		return (NULL);
 
 	new_table->size = size;
 
