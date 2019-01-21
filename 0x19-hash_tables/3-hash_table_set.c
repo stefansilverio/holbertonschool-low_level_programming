59cafd33fc6b33c8c5e8171e949ae626ab20e4c3
stefansilverio <494@holbertonschool.com>
2019-01-21 18:45:13 +0000

    set hash table

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -24,13 +24,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	curr = malloc(sizeof(hash_node_t));
 	if (curr == NULL)
 		return (0);
-	mov = malloc(sizeof(hash_node_t));
-	if (mov == NULL)
-		return (0);
 	hash = hash_djb2((const unsigned char *)key);
 	index = hash % (ht->size);
-	curr->key = (char *) strdup(key);
-	curr->value = (char *) strdup(value);
+	curr->key = strdup(key);
+	curr->value = strdup(value);
 	curr->next = NULL;
 
 	if (ht->array[index] != 0)
@@ -40,8 +37,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			if ((strcmp(mov->key, curr->key)) == 0)
 			{
+				free(mov->value);
 				mov->value = strdup(value);
-				/* free stuff */
+				free(curr->key);
+				free(curr->value);
+				free(curr);
 				return (1);
 			}
 			mov = mov->next;
