50bd3de1d7387e47ebfd847609b1551ac2c6338e
Rmolimock <495@holbertonschool.com>
2019-02-08 03:39:10 +0000

    betty compliant

--- 0x1A-sorting_algorithms/102-counting_sort.c
+++ 0x1A-sorting_algorithms/102-counting_sort.c
@@ -13,7 +13,6 @@ void counting_sort(int *array, size_t size)
 	int *counter, *output;
 	size_t i, largest = 0;
 
-	/*create counter array*/
 	for (i = 0; i < size; i++)
 	{
 		if (array[i] > (int)largest)
@@ -21,38 +20,19 @@ void counting_sort(int *array, size_t size)
 	}
 	largest++;
 	counter = malloc((largest + 1) * sizeof(int));
-	/*insert number of each array value into counter array*/
 	for (i = 0; i < size; i++)
-	{
 		counter[array[i]] += 1;
-	}
-	
-	/*add previous values along counter array*/
 	for (i = 1; i <= largest; i++)
-	{
 		counter[i] = counter[i - 1] + counter[i];
-	}
-
-	/*print counter array*/
 	print_array(counter, largest);
-
-	/*create output array*/
 	output = malloc(size * sizeof(int));
-
-	/*assign values to output array*/
 	for (i = 0; i < size; i++)
 	{
 		output[counter[array[i]] - 1] = array[i];
 		counter[array[i]] -= 1;
 	}
-	
-	/*update array with out values*/
 	for (i = 0; i < size; i++)
-	{
 		array[i] = output[i];
-	}
-
-	/*free counter and output array memory*/
 	free(counter);
 	free(output);
 }
