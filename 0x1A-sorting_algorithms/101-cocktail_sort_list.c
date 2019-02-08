dfe9da3bef50f664e8713e533dfb8ba290ae71b9
stefansilverio <494@holbertonschool.com>
2019-02-08 16:36:17 +0000

    dumb mistake fixes

--- 0x1A-sorting_algorithms/101-cocktail_sort_list.c
+++ 0x1A-sorting_algorithms/101-cocktail_sort_list.c
@@ -11,6 +11,9 @@ void cocktail_sort_list(listint_t **list)
 	int sorted = 0;
 	listint_t *node = *list;
 
+	if (list == NULL || (*list)->next == NULL)
+		return;
+
 	while (!sorted)
 	{
 		sorted = 1;
