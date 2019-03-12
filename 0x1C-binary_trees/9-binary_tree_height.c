ff5aa9dcf377b3580bf98977b9b96f749e7cacbf
stefansilverio <494@holbertonschool.com>
2019-03-12 21:22:15 +0000

    determine height

---
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int hr = 0, hl = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left)
+		hl = binary_tree_height(tree->left) + 1;
+	binary_tree_height(tree->left);
+	if (tree->right)
+		hr = binary_tree_height(tree->right) + 1;
+	binary_tree_height(tree->right);
+	if (hl > hr)
+		return (hl);
+	return (hr);
+}
