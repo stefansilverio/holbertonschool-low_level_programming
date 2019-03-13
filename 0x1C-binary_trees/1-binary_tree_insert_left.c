b76c39d5e378270e4ec04dfa5f0327f3cc21f8e7
stefansilverio <494@holbertonschool.com>
2019-03-13 21:18:57 +0000

    set left

--- 0x1C-binary_trees/1-binary_tree_insert_left.c
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 	binary_tree_t *curr = NULL;
 
-	if (new_node == NULL)
+	if (new_node == NULL || parent == NULL)
 		return (NULL);
 
 	new_node->n = value;
