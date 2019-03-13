144eb7837f5ba16a423107570eee7de3f7d6274b
stefansilverio <494@holbertonschool.com>
2019-03-13 21:46:00 +0000

    traverse tree in order

--- 0x1C-binary_trees/7-binary_tree_inorder.c
+++ 0x1C-binary_trees/7-binary_tree_inorder.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_inorder - traverse tree in order
+ * @tree: ptr to root
+ * @func: print function
+ * Return: nothing
+ */
+
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
