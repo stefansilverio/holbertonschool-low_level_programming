61c3dcfcedbc8531392dd63f81dbb735ad377682
stefansilverio <494@holbertonschool.com>
2019-03-13 21:51:01 +0000

    determine height of binary tree

--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_height - check height of binary tree
+ * @tree: ptr to root node of tree
+ * Return: height
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	int hr = 0, hl = 0;
