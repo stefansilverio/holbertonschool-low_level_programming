44a4311c574bf73bad0488b5e976b447de679c01
stefansilverio <494@holbertonschool.com>
2019-03-13 21:37:37 +0000

    check if node is leaf

--- 0x1C-binary_trees/4-binary_tree_is_leaf.c
+++ 0x1C-binary_trees/4-binary_tree_is_leaf.c
@@ -1,4 +1,9 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - check if node is leaf
+ * @node: ptr to node to check
+ * Return: return 1 if leaf 0 if not
+ */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
