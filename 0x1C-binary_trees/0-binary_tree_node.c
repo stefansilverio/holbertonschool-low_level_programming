b5a2169c98d5aa0a6a4467970f8d8ac3d4b636f5
stefansilverio <494@holbertonschool.com>
2019-03-13 20:10:43 +0000

    add node

--- 0x1C-binary_trees/0-binary_tree_node.c
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -1,8 +1,14 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_node - add node
+ * @parent: ptr to node to create
+ * @value: value to put in node
+ * Return: ptr to new_node
+ */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t* new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
 	if (new_node == NULL)
 		return (NULL);
