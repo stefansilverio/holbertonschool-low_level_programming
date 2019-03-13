9e524769a5eb0a7e774d29a108217552025b988d
stefansilverio <494@holbertonschool.com>
2019-03-13 21:52:59 +0000

    determine depth of bin tree

--- 0x1C-binary_trees/10-binary_tree_depth.c
+++ 0x1C-binary_trees/10-binary_tree_depth.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_depth - determine depth
+ * @tree: ptr to node to measure depth
+ * Return: depth
+ */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	int depth = 0;
