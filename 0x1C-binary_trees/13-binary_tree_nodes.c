a6c96f29e43575c42bd72a8bb6ef149993873d06
stefansilverio <494@holbertonschool.com>
2019-03-13 22:00:08 +0000

    count nodes w/ at least 1 child

--- 0x1C-binary_trees/13-binary_tree_nodes.c
+++ 0x1C-binary_trees/13-binary_tree_nodes.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_nodes - count nodes w/ >= 1 child
+ * @tree: ptr to root node of bin tree
+ * Return: # nodes w/ >= 1 child
+ */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	int child1 = 0, child2 = 0;
@@ -10,5 +15,5 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 	child1 = binary_tree_nodes(tree->left);
 	child2 = binary_tree_nodes(tree->right);
-	return (child1 + child2) + 1;
+	return ((child1 + child2) + 1);
 }
