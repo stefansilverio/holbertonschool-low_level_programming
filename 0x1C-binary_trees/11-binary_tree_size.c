724c04aab705a437c8d9a9471c8e13aa0096e467
stefansilverio <494@holbertonschool.com>
2019-03-13 21:55:11 +0000

    determine size of bin tree

--- 0x1C-binary_trees/11-binary_tree_size.c
+++ 0x1C-binary_trees/11-binary_tree_size.c
@@ -1,12 +1,16 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_size - determine size of bin tree
+ * @tree: ptr to root node
+ * Return: size of bin tree
+ */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	int count = 1;
 
 	if (tree == NULL)
 		return (0);
-
 	count = count + binary_tree_size(tree->left);
 	count = count + binary_tree_size(tree->right);
 	return (count);
