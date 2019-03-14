0641eed29a4f4e41bde8e8ff3263778f4d1b9535
stefansilverio <494@holbertonschool.com>
2019-03-14 03:06:55 +0000

    determine if perfect bin tree

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -10,6 +10,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
 	if (tree->left)
 		hl = binary_tree_height(tree->left);
 	if (tree->right)
@@ -45,12 +47,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-        if (tree == NULL)
-                return (0);
-        if (!tree->left && !tree->right)
-                return (1);
-        if ((tree->left) && (tree->right))
-                return (binary_tree_is_full(tree->left) &&
-                        binary_tree_is_full(tree->right));
-        return (0);
+	if (tree == NULL)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+	if ((tree->left) && (tree->right))
+		return (binary_tree_is_full(tree->left) &&
+			binary_tree_is_full(tree->right));
+	return (0);
 }
