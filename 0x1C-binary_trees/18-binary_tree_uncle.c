dabf45d3ef49fa42caf12c2307d1e53d4d744639
stefansilverio <494@holbertonschool.com>
2019-03-14 02:52:15 +0000

    find uncle nodes

---
+++ 0x1C-binary_trees/18-binary_tree_uncle.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL ||
+	    node->parent->parent == NULL)
+		return (NULL);
+	node = node->parent;
+	if (node->parent->left != NULL && node->parent->left != node)
+		return (node->parent->left);
+	if (node->parent->right != NULL && node->parent->right != node)
+		return (node->parent->right);
+	return (NULL);
+}
