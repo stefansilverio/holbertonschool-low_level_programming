a9bcc0dddaa363720c68bc6b1fe74692a92d5623
stefansilverio <494@holbertonschool.com>
2019-03-15 16:15:04 +0000

    find lowest common ancestor

--- 0x1C-binary_trees/100-binary_trees_ancestor.c
+++ 0x1C-binary_trees/100-binary_trees_ancestor.c
@@ -15,6 +15,9 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 	if (!first || !second)
 		return (NULL);
 
+	if (first == second)
+		return ((binary_tree_t *)first);
+
 	if (first == second->parent)
 		return ((binary_tree_t *)first);
 
