91f3bdce917075f42dcf6bf9dac08a51fa3f0558
stefansilverio <494@holbertonschool.com>
2019-04-18 16:33:07 +0000

    updated prototypes

--- 0x1D-search_algorithms/search_algos.h
+++ 0x1D-search_algorithms/search_algos.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 int jump_search(int *array, size_t size, int value);
+int _search(int *array, short jump, int size, int hi, int low, int value);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int _split(int *array, size_t hi, size_t low, int value);
